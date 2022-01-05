aabb32bab2425b09788397a754d0168360ca2dcf
NirmalkumarKGS <kgsnirmalkumarr5285@gmail.com>
2022-01-05 23:49:50 +0530

    Rotation Count
    Binary Search Problem

--- Algorithms/src/searching/RotateCount.java
+++ Algorithms/src/searching/RotateCount.java
@@ -1,29 +1,43 @@
 package searching;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RotateCount {
 
 	public static void main(String[] args) {
-		int[] arr = {15,21,27,2,3,6,8,10,13};
-		System.out.println(binarySearch(arr) + 1);
+		int[] nums = { 15, 21, 27, 2, 3, 6, 8, 10, 13 };
+		int target =5;
+		// System.out.println(binarySearch(arr) + 1);
+		Map<Integer, Integer> set = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			int diff = target - nums[i];
+			if (set.containsKey(diff)) {
+				System.out.println(set.get(diff));System.out.println(i);
+				System.out.println(nums[set.get(diff)]);System.out.println(nums[i]);
+			} else {
+				set.put(nums[i], i);
+			}
+		}
 		
-		//expected output is 3
+		// expected output is 3
 	}
-	
+
 	public static int binarySearch(int[] arr) {
 		int start = 0;
 		int end = arr.length - 1;
-		while(start <= end) {			
-			int mid = start + (end - start)/2;
-			if(mid < end && arr[mid] > arr[mid+1]) {
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (mid < end && arr[mid] > arr[mid + 1]) {
 				return mid;
 			}
-			if(mid > start && arr[mid] < arr[mid-1]) {
-				return mid-1;
+			if (mid > start && arr[mid] < arr[mid - 1]) {
+				return mid - 1;
 			}
-			if(arr[mid] <= arr[start]) {
-				end = mid-1;
+			if (arr[mid] <= arr[start]) {
+				end = mid - 1;
 			} else {
-				start = mid+1;
+				start = mid + 1;
 			}
 		}
 		return -1;
